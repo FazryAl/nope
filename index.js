6d1397d1168144d5f8022236a754df860bb4248c
FazryAl <146300440+FazryAl@users.noreply.github.com>
2024-01-18 16:20:09 +0700

    index.js

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
-document.getElementById("start").addEventListener("click", function(){
-    for (let i = 0; 1 < 100000000; i++) {}
-})
-
+document.getElementById("start").addEventListener("click", function() {
+    for (let i = 0; i < 100; i++) {
+        const particle = document.createElement("div");
+        particle.classList.add("particle");
+        document.getElementById("particles").appendChild(particle);
+    }
+});
